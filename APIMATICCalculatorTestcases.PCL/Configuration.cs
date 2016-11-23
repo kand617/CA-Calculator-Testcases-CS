25ec15446a3821242aa7ea403f563c520a70db72
kand617 <git@apimatic.io>
2016-11-23 22:13:27 +0000

    Initial commit

---
+++ APIMATICCalculatorTestcases.PCL/Configuration.cs
@@ -0,0 +1,18 @@
+
+namespace APIMATICCalculatorTestcases.PCL
+{
+    public partial class Configuration
+    {
+        //The base Uri for API calls
+        public static string BaseUri = "http://examples.apimatic.io/apps/calculator";
+
+        //The username to use with basic authentication
+        //TODO: Replace the BasicAuthUserName with an appropriate value
+        public static string BasicAuthUserName = "TODO: Replace";
+
+        //The password to use with basic authentication
+        //TODO: Replace the BasicAuthPassword with an appropriate value
+        public static string BasicAuthPassword = "TODO: Replace";
+
+    }
+}
